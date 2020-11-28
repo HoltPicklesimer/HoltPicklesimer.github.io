705457e953dda9c37eec65f819c650fb7b468c84
Holt Picklesimer <holtpicklesimer@gmail.com>
2020-11-28 12:55:29 -0700

    Fix scaling issue

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,10 +12,10 @@ function Hero(props) {
             <Container fluid={true}>
                 <Row className="justify-content-center py-5">
                     <Col md={1.5} sm={1.5}>
-                        { props.img && <Image src={props.img} fuid={true} roundedCircle={true} width={150} height={150} style={{padding:"10px"}} />}
+                        { props.img && <Image src={props.img} fluid={true} roundedCircle={true} width={150} height={150} style={{padding:"10px"}} />}
                     </Col>
                     <Col md={10.5} sm={10.5}>
-                        { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
+                        { props.title && <h1 className="display-2 font-weight-bolder">{props.title}</h1>}
                         { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3>}
                         { props.text && <h3 className="lead font-weight-light">{props.text}</h3>}
                     </Col>
